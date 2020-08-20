62a5d8ccec1817317f829edcf3b252fb43d68c1d
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-08-20 16:32:08 +0200

    simplifies example

--- example/src/App.js
+++ example/src/App.js
@@ -1,41 +1,45 @@
-import React, { Suspense } from "react";
-import { Canvas } from "react-three-fiber";
-
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { Octahedron } from "drei";
 import { Recorder, useCapture } from "use-capture";
 
-import Scene from "./Scene";
+function Scene() {
+  const { getProgress } = useCapture();
+
+  const mesh = useRef();
+  useFrame(() => {
+    mesh.current.rotation.x = getProgress() * Math.PI * 2;
+    mesh.current.rotation.y = getProgress() * Math.PI * 4;
+  });
+
+  return (
+    <Octahedron ref={mesh} args={[2]} rotation={[Math.PI / 2, 0, 0]}>
+      <meshNormalMaterial flatShadings />
+    </Octahedron>
+  );
+}
 
 function App() {
   const { startRecording, isRecording } = useCapture();
 
   return (
     <>
-      <div
-        className={`recording ${isRecording && "active"}`}
-        onClick={() => startRecording()}
-      />
+      <button className="recording" onClick={startRecording}>
+        {isRecording ? "Recording..." : "Start Recording"}
+      </button>
       <Canvas
-        shadowMap
-        colorManagement
         // 💡 preserveDrawingBuffer is mandatory
         gl={{
           preserveDrawingBuffer: true,
         }}
-        // 💡 not having a clear color would glitch the recording
-        onCreated={({ gl }) => {
-          gl.setClearColor("#fff");
-        }}
-        concurrent
       >
-        <Suspense fallback={null}>
-          <Scene />
-        </Suspense>
-
+        {/* 💡 not having a clear color would glitch the recording */}
+        <color attach="background" args={["#000"]} />
+        <Scene />
         <Recorder
-          duration={4}
-          framerate={24}
-          motionBlurFrames={0}
-          format={"webm"}
+          duration={2}
+          framerate={60}
+          motionBlurFrames={1}
           filename={"my-recording"}
         />
       </Canvas>
